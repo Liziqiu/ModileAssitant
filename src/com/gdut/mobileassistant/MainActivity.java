99759bc65057b4e168ca1467834fe9cba1c761a7
Liziqiu <liziqiu_89@126.com>
2015-03-27 10:28:10 +0800

    config the control center button in layout and main activity

--- src/com/gdut/mobileassistant/MainActivity.java
+++ src/com/gdut/mobileassistant/MainActivity.java
@@ -3,20 +3,31 @@ package com.gdut.mobileassistant;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
+	private Button ControlCenter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initResources();
+	}
+
+	private void initResources() {
+		ControlCenter = (Button) this.findViewById(R.id.control_center);
+		ControlCenter.setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		if(v == ControlCenter){
+			
+		}
+		
 	}
 
 }
